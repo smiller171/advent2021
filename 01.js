7c359fce377345cfa4475140b6135f4f4e78b551
Scott Miller <scott@millergeek.xyz>
2021-12-03 11:25:39 -0500

    day 1 part 2

--- 01.js
+++ 01.js
@@ -3,10 +3,20 @@ const fs = require('fs')
 
 let countDepthIncreases = (prev, curr, index, array) => curr > array[ index - 1 ] ? prev + 1 : prev
 
+let inputArray = fs.readFileSync('inputs/01.txt')
+  .toString()
+  .split('\n')
+  .map(Number)
+
 console.log(
-  fs.readFileSync('inputs/01.txt')
-    .toString()
-    .split('\n')
-    .map(Number)
-    .reduce(countDepthIncreases, 0)
+  inputArray.reduce(countDepthIncreases, 0)
+)
+
+let getSlidingWindows = array => array
+  .map((element, index) => element + array[ index + 1 ] + array[ index + 2 ])
+  .slice(0, -3)
+
+let slidingArray = getSlidingWindows(inputArray)
+console.log(
+  slidingArray.reduce(countDepthIncreases, 0)
 )
