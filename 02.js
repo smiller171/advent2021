f8917a24ef64f9b4346da164dc7f6b4769139857
Scott Miller <scott@millergeek.xyz>
2021-12-03 13:05:18 -0500

    day 2 part 2

--- 02.js
+++ 02.js
@@ -31,8 +31,30 @@ let positionReducer = (tallyObject, instruction) => {
   return tallyObject
 }
 
+let aimedPositionReducer = (tallyObject, instruction) => {
+  switch (instruction.direction) {
+    case 'forward':
+      tallyObject.horizontal += instruction.distance
+      tallyObject.depth += instruction.distance * tallyObject.aim
+      break
+    case 'down':
+      tallyObject.aim += instruction.distance
+      break
+    case 'up':
+      tallyObject.aim -= instruction.distance
+      break
+    default:
+      console.error('invalid instruction: ', instruction)
+  }
+  return tallyObject
+}
+
 let structuredArray = inputArray.map(getMovementObject)
 let finalPosition = structuredArray.reduce(positionReducer, { 'horizontal': 0, 'depth': 0 })
+let aimedFinalPosition = structuredArray.reduce(aimedPositionReducer, { 'horizontal': 0, 'depth': 0, 'aim': 0 })
 
 console.log('Final position is:', finalPosition)
 console.log('Product is:', finalPosition.horizontal * finalPosition.depth)
+
+console.log('Final aimed position is:', aimedFinalPosition)
+console.log('Product is:', aimedFinalPosition.horizontal * aimedFinalPosition.depth)
