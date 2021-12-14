905fb7d6bdf79ad55c2a4fd39aa643d855dcb39f
Scott Miller <scott@millergeek.xyz>
2021-12-14 15:33:48 -0500

    solved day 5.2

--- 05.js
+++ 05.js
@@ -3,49 +3,75 @@ import Data from './dataimport'
 class VentLine {
   constructor(string) {
     let stringList = string.split(' -> ')
-    this.points = stringList.map(e => e.split(',').map(Number))
-  }
-}
-
-class VentLines {
-  constructor(inputList) {
-    this.list = inputList.map(e => new VentLine(e).points)
+    let endpoints = stringList.map(e => e.split(',').map(Number))
+    this.isVertical = endpoints[ 0 ][ 0 ] === endpoints[ 1 ][ 0 ]
+    this.isHorizontal = endpoints[ 0 ][ 1 ] === endpoints[ 1 ][ 1 ]
+    this.isDiagonal = !(this.isHorizontal || this.isVertical)
+    this.points = new Set
+    this.xlimit = endpoints.reduce((a, b) => a[ 0 ] > b[ 0 ] ? a[ 0 ] : b[ 0 ])
+    this.ylimit = endpoints.reduce((a, b) => a[ 1 ] > b[ 1 ] ? a[ 1 ] : b[ 1 ])
+    if (this.isHorizontal) {
+      let y = endpoints[ 0 ][ 1 ]
+      let sorted = [ ...endpoints ].sort((a, b) => a[ 0 ] - b[ 0 ])
+      for (let x = sorted[ 0 ][ 0 ]; x <= sorted[ 1 ][ 0 ]; x++) {
+        this.points.add([ x, y ])
+      }
+    } else if (this.isVertical) {
+      let x = endpoints[ 0 ][ 0 ]
+      let sorted = [ ...endpoints ].sort((a, b) => a[ 1 ] - b[ 1 ])
+      for (let y = sorted[ 0 ][ 1 ]; y <= sorted[ 1 ][ 1 ]; y++) {
+        this.points.add([ x, y ])
+      }
+    } else {
+      let sorted = [ ...endpoints ].sort((a, b) => a[ 0 ] - b[ 0 ])
+      // after sorting so that X is always increasing, try both increasing and decreasing for y
+      for (let y = sorted[ 0 ][ 1 ], x = sorted[ 0 ][ 0 ]; y <= sorted[ 1 ][ 1 ]; y++, x++) {
+        this.points.add([ x, y ])
+      }
+      for (let y = sorted[ 0 ][ 1 ], x = sorted[ 0 ][ 0 ]; y >= sorted[ 1 ][ 1 ]; y--, x++) {
+        this.points.add([ x, y ])
+      }
+    }
   }
 }
 
 class VentGrid {
-  constructor(ventLines) {
+  constructor(inputArray) {
+    let ventLines = inputArray.map(e => new VentLine(e))
     let clone = (items) => items.map(item => Array.isArray(item) ? clone(item) : item)
-    let isVertical = line => line[ 0 ][ 0 ] === line[ 1 ][ 0 ]
-    let isHorizontal = line => line[ 0 ][ 1 ] === line[ 1 ][ 1 ]
-    let xlimit = ventLines.flat().reduce((prev, curr) => prev[ 0 ] > curr[ 0 ] ? prev : curr)[ 0 ] + 1
-    let ylimit = ventLines.flat().reduce((prev, curr) => prev[ 1 ] > curr[ 1 ] ? prev : curr)[ 1 ] + 1
+    let xlimit = ventLines.reduce((prev, curr) => prev.xlimit > curr.xlimit ? prev : curr).xlimit + 1
+    let ylimit = ventLines.reduce((prev, curr) => prev.ylimit > curr.ylimit ? prev : curr).ylimit + 1
     let gridTemplate = new Array(xlimit).fill(new Array(ylimit).fill(0))
-    let grid = clone(gridTemplate)
+    // Deep cloning the template so that each row isn't a ref to the same Array
+    let horizontalGrid = clone(gridTemplate)
     ventLines.forEach(line => {
-      if (isHorizontal(line)) {
-        let y = line[ 0 ][ 1 ]
-        let sorted = [ ...line ].sort((a, b) => a[ 0 ] - b[ 0 ])
-        for (let x = sorted[ 0 ][ 0 ]; x <= sorted[ 1 ][ 0 ]; x++) {
-          grid[ y ][ x ] = grid[ y ][ x ] + 1
-        }
-      }
-      if (isVertical(line)) {
-        let x = line[ 0 ][ 0 ]
-        let sorted = [ ...line ].sort((a, b) => a[ 1 ] - b[ 1 ])
-        for (let y = sorted[ 0 ][ 1 ]; y <= sorted[ 1 ][ 1 ]; y++) {
-          grid[ y ][ x ] = grid[ y ][ x ] + 1
-        }
+      if (!line.isDiagonal) {
+        line.points.forEach(point => {
+          let x = point[ 0 ]
+          let y = point[ 1 ]
+          horizontalGrid[ y ][ x ]++
+        })
       }
     })
+
+    let grid = clone(gridTemplate)
+    ventLines.forEach(line => {
+      line.points.forEach(point => {
+        let x = point[ 0 ]
+        let y = point[ 1 ]
+        grid[ y ][ x ]++
+      })
+    })
+    let gridReducer = (count, curr) => curr > 1 ? count + 1 : count
+    this.horizontalGrid = horizontalGrid
     this.grid = grid
-    this.overlapCount = grid.flat(2).reduce((count, curr) => curr > 1 ? count + 1 : count, 0)
+    this.horizontalOverlapCount = horizontalGrid.flat(2).reduce(gridReducer, 0)
+    this.overlapCount = grid.flat(2).reduce(gridReducer, 0)
   }
 }
 
 let inputArray = new Data('inputs/05.txt').inputArray
-let ventLines = new VentLines(inputArray).list
-let grid = new VentGrid(ventLines)
-console.log(grid.overlapCount)
+let grid = new VentGrid(inputArray)
+console.log(grid.horizontalOverlapCount, grid.overlapCount)
 
-export { VentLine, VentLines, VentGrid }
+export { VentLine, VentGrid }
--- tests/05.test.js
+++ tests/05.test.js
@@ -1,36 +1,60 @@
 import * as solution from '../05'
 import Data from '../dataimport'
 let testData = new Data('tests/05.txt').inputArray
-let testLines = new solution.VentLines(testData).list
-let testGrid = new solution.VentGrid(testLines)
+let testGrid = new solution.VentGrid(testData)
 
 describe('Vent Lines', () => {
-  test('Can create an accurate vent line', () => {
-    let testLine = new solution.VentLine('0,9 -> 5,9')
-    expect(testLine.points).toEqual([ [ 0, 9 ], [ 5, 9 ] ])
+  test('Can create an accurate horizontal vent line', () => {
+    let line = new solution.VentLine('0,9 -> 5,9')
+    expect(line.isHorizontal).toEqual(true)
+    expect(line.isVertical).toEqual(false)
+    expect(line.isDiagonal).toEqual(false)
+    expect(line.points).toMatchObject(new Set([
+      [ 0, 9 ],
+      [ 1, 9 ],
+      [ 2, 9 ],
+      [ 3, 9 ],
+      [ 4, 9 ],
+      [ 5, 9 ]
+    ]))
   })
-  debugger
-  test('should create a list of VentLine objects', () => {
-    expect(testLines).toEqual([
-      [ [ 0, 9 ], [ 5, 9 ] ],
-      [ [ 8, 0 ], [ 0, 8 ] ],
-      [ [ 9, 4 ], [ 3, 4 ] ],
-      [ [ 2, 2 ], [ 2, 1 ] ],
-      [ [ 7, 0 ], [ 7, 4 ] ],
-      [ [ 6, 4 ], [ 2, 0 ] ],
-      [ [ 0, 9 ], [ 2, 9 ] ],
-      [ [ 3, 4 ], [ 1, 4 ] ],
-      [ [ 0, 0 ], [ 8, 8 ] ],
-      [ [ 5, 5 ], [ 8, 2 ] ]
-    ])
+  test('Can create an accurate vertical vent line', () => {
+    let line = new solution.VentLine('7,0 -> 7,4')
+    expect(line.isHorizontal).toEqual(false)
+    expect(line.isVertical).toEqual(true)
+    expect(line.isDiagonal).toEqual(false)
+    expect(line.points).toMatchObject(new Set([
+      [ 7, 0 ],
+      [ 7, 1 ],
+      [ 7, 2 ],
+      [ 7, 3 ],
+      [ 7, 4 ]
+    ]))
+  })
+  test('Can create an accurate diagonal vent line', () => {
+    let line = new solution.VentLine('8,0 -> 0,8')
+    expect(line.isHorizontal).toEqual(false)
+    expect(line.isVertical).toEqual(false)
+    expect(line.isDiagonal).toEqual(true)
+    expect(line.points).toMatchObject(new Set([
+      [ 8, 0 ],
+      [ 7, 1 ],
+      [ 6, 2 ],
+      [ 5, 3 ],
+      [ 4, 4 ],
+      [ 3, 5 ],
+      [ 2, 6 ],
+      [ 1, 7 ],
+      [ 0, 8 ]
+    ]))
   })
 })
 
-describe('Vent Grid', () => {
+describe('Horizontal Grid', () => {
   test('should be able to produce a grid', () => {
-    expect(testGrid.grid.length).toEqual(10)
-    expect(testGrid.grid[ 0 ].length).toEqual(10)
-    expect(testGrid.grid.map(e => e.toString())).toEqual([
+    expect(testGrid.horizontalGrid.length).toEqual(10)
+    expect(testGrid.horizontalGrid[ 0 ].length).toEqual(10)
+    expect(testGrid.horizontalGrid.map(e => e.toString())).toEqual([
       '0,0,0,0,0,0,0,1,0,0',
       '0,0,1,0,0,0,0,1,0,0',
       '0,0,1,0,0,0,0,1,0,0',
@@ -44,6 +68,28 @@ describe('Vent Grid', () => {
     ])
   })
   test('should be able to count overlap points', () => {
-    expect(testGrid.overlapCount).toEqual(5)
+    expect(testGrid.horizontalOverlapCount).toEqual(5)
+  })
+})
+
+describe('Full Grid', () => {
+  test('should be able to produce a grid', () => {
+    expect(testGrid.grid.length).toEqual(10)
+    expect(testGrid.grid[ 0 ].length).toEqual(10)
+    expect(testGrid.grid.map(e => e.toString())).toEqual([
+      "1,0,1,0,0,0,0,1,1,0",
+      "0,1,1,1,0,0,0,2,0,0",
+      "0,0,2,0,1,0,1,1,1,0",
+      "0,0,0,1,0,2,0,2,0,0",
+      "0,1,1,2,3,1,3,2,1,1",
+      "0,0,0,1,0,2,0,0,0,0",
+      "0,0,1,0,0,0,1,0,0,0",
+      "0,1,0,0,0,0,0,1,0,0",
+      "1,0,0,0,0,0,0,0,1,0",
+      "2,2,2,1,1,1,0,0,0,0"
+    ])
+  })
+  test('should be able to count overlap points', () => {
+    expect(testGrid.overlapCount).toEqual(12)
   })
 })
